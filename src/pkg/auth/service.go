e648f7374a097b0205eb1f4e4df6b337e73289c3
icowan <solacowa@gmail.com>
2020-10-30 17:28:22 +0800

    增加代理使用

--- src/pkg/auth/service.go
+++ src/pkg/auth/service.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"context"
+	"crypto/tls"
 	"encoding/base64"
 	"errors"
 	"github.com/dgrijalva/jwt-go"
@@ -20,6 +21,7 @@ import (
 	oauthgithub "golang.org/x/oauth2/github"
 	"gopkg.in/guregu/null.v3"
 	"math/rand"
+	"net"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -131,6 +133,25 @@ func (c *service) AuthLoginGithubCallback(w http.ResponseWriter, r *http.Request
 		resp.Err = errors.New("token is nil or token.valid is false")
 		_ = encodeLoginResponse(ctx, w, resp)
 		return
+	}
+
+	if httpProxy := c.config.GetString(config.SectionServer, "http_proxy"); httpProxy != "" {
+		_ = level.Debug(c.logger).Log("use-proxy", httpProxy)
+		dialer := &net.Dialer{
+			Timeout:   time.Duration(5 * int64(time.Second)),
+			KeepAlive: time.Duration(5 * int64(time.Second)),
+		}
+		ctx = context.WithValue(ctx, oauth2.HTTPClient, &http.Client{
+			Transport: &http.Transport{
+				Proxy: func(_ *http.Request) (*url.URL, error) {
+					return url.Parse(httpProxy)
+				},
+				DialContext: dialer.DialContext,
+				TLSClientConfig: &tls.Config{
+					InsecureSkipVerify: false,
+				},
+			},
+		})
 	}
 
 	client := github.NewClient(githubOauthConfig.Client(ctx, token))
